9a4027d02afd83ba4e6cc7f644bba3d37ed4d32d
CuentaNoDefinitivo <pentakill3435@gmail.com>
2022-07-31 21:35:07 +0200

    corregido

--- DISPAROS TEMPORIZADOS/Assets/Script/Bullet.cs
+++ DISPAROS TEMPORIZADOS/Assets/Script/Bullet.cs
@@ -9,13 +9,12 @@ public class Bullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, destroyTime);
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
-        Destroy(gameObject, destroyTime);
     }
 }
